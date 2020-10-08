230f683da2dc7e96dc3c2b0c9e7d31e9093781f3
Luca Armaroli <lukaarma@gmail.com>
2020-10-08 11:34:48 +0200

    changed default imports

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
+import './css/style.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+
+createApp(App).use(router).mount('#app');
