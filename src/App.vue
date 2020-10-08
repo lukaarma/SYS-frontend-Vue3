1af760d63722ab4373479a325bf929b0f70c77a4
Luca Armaroli <lukaarma@gmail.com>
2020-10-08 11:35:23 +0200

    changed default rout and view

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <Navbar />
+    <router-view />
 </template>
+
+
+<script lang="ts">
+import Navbar from "./components/Navbar.vue";
+
+export default {
+    components: {
+        Navbar,
+    },
+};
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,52 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
 
+/* SITEMAP
+/home
+    /profile
+        /reservations   USER ONLY   manage reservations
+            /edit       USER ONLY   edit reservation
+
+        /admin          ADMIN ONLY  manage admin tools
+            /create     ADMIN ONLY  create room
+            /edit       ADMIN ONLY  manage room
+
+        /setting        account settings
+
+
+    /rooms              browse Spots
+        /reserve        USER ONLY create reservation
+*/
+
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        redirect: '/home'
+    },
+    {
+        path: '/home',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+        path: '/rooms',
+        name: 'Rooms',
+        component: () => import(/* webpackChunkName: "rooms" */ '../views/Rooms.vue')
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+});
 
-export default router
+export default router;
